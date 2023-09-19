5bfa7bf13bdce528131bc98b8f06b0593b63b1ec
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-19 13:52:19 +0300

    trying to solve the carousel problem

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -6,7 +6,54 @@ import '../App.css';
 const CarouselComponent = () => {
     return (
         <div className='container-fluid'>
-            <Carousel>
+            <div class="carousel slide">
+                <div class="carousel-inner">
+                    <ol class="carousel-indicators">
+                        <li data-mdb-target="0" class=""></li>
+                        <li data-mdb-target="1" class=""></li>
+                        <li data-mdb-target="2" class="active"></li>
+                    </ol>
+                    <div class="carousel-item">
+                        <img class="w-100 d-block" src="./carousel-1.jpg" alt="..." />
+                        <div class="carousel-caption d-none d-md-block">
+                            <div class="position-absolute top-50  align-items-center">
+                                <h6 class="text-light fw-bolder ">Providing you</h6>
+                                <h1><span class="blue-text">Secure</span> &amp; <span class="blue-text">Reliable</span> e-Payment Platform</h1>
+                                <p class="fs-5 text">Experience the future of hassle-free e-payments with Kimipay's cutting-edge solutions that set new standards for security and reliability.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="w-100 d-block" src="./carousel-2.jpg" alt="..." />
+                            <div class="carousel-caption d-none d-md-block">
+                                <div class="position-absolute top-50  align-items-center">
+                                    <h6 class="text-light fw-bolder ">We are leaders in</h6>
+                                    <h1><span class="blue-text">Plug</span> &amp; <span class="blue-text">Play</span> Solutions</h1>
+                                    <p class="fs-5 text">At Kimipay, we simplify your e-payment needs, offering intuitive solutions that guarantee both safety and dependability</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item active">
+                                <img class="w-100 d-block" src="./carousel-3.jpg" alt="..." />
+                                <div class="carousel-caption d-none d-md-block">
+                                    <div class="position-absolute top-50  align-items-center">
+                                        <h6 class="text-light fw-bolder">We offer the</h6>
+                                        <h1><span class="blue-text">Fastest</span> SMS Speeds</h1>
+                                        <p class="fs-5 text">Unlock the swiftness of out bulk SMS services.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a role="button" class="carousel-control-prev">
+                                    <span class="carousel-control-prev-icon"></span>
+                                    <span class="visually-hidden"></span>
+                                </a>
+                                <a role="button" class="carousel-control-next">
+                                    <span class="carousel-control-next-icon"></span>
+                                    <span class="visually-hidden"></span>
+                                </a>
+                            </div>
+                        </div>
+            {/* <Carousel>
             <Carousel.Item interval={3500}>
                 <ExampleCarouselImage
                 text="First slide"
@@ -43,7 +90,7 @@ const CarouselComponent = () => {
                 <p>Unlock the swiftness of out bulk SMS services.</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            </Carousel>
+            </Carousel> */}
         </div>
     );
 };
