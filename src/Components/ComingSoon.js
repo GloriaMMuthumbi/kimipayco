b6f551cfa78da2d2957a05c855098ac448eaa477
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-23 01:44:14 +0300

    added coming soon component in register page

--- src/Components/ComingSoon.js
+++ src/Components/ComingSoon.js
@@ -4,8 +4,13 @@ const ComingSoonComponent = () => {
     return ( 
         <div className="container-fluid">
             <div className="row coming-soon-container">
-                <div className="col-4">
+                <div className="col-4 coming-soon-text">
                     <h1 className="coming-soon-heading">Coming soon...</h1>
+                    <p>
+                        This page is currently under construction. We are sorry for 
+                        any inconvinienced caused.
+                    </p>
+                    <a href="/">Go Back</a>
                 </div>
                 <div className="col-4">
                     <img
