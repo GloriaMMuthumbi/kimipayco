a64a17ac2a1f61c22f95420d96c56f91cb0639cc
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-23 02:22:47 +0300

    fixing responsiveness of the coming soon component

--- src/Components/ComingSoon.js
+++ src/Components/ComingSoon.js
@@ -4,7 +4,13 @@ const ComingSoonComponent = () => {
     return ( 
         <div className="container-fluid">
             <div className="row coming-soon-container">
-                <div className="col-4 coming-soon-text">
+                <div className=" col-4 col-md-3 col-sm-12 coming-soon-text">
+                <img
+                        src="./logo22.png"
+                        width="200px"
+                        height="auto"
+                        alt="logo"
+                        />
                     <h1 className="coming-soon-heading">Coming soon...</h1>
                     <p>
                         This page is currently under construction. We are sorry for 
@@ -12,10 +18,10 @@ const ComingSoonComponent = () => {
                     </p>
                     <a href="/">Go Back</a>
                 </div>
-                <div className="col-4">
+                <div className="col-md-3 col-sm-12 image">
                     <img
                         src="./cooming-soon.svg"
-                        width="500px"
+                        width="450px"
                         height="auto"
                         alt="coming soon"
                         />
